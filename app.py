9484de622f0b7bd10854c791512e5921bdc71e59
muoegu <kmicah1794@gmail.com>
2022-09-29 09:58:39 +0800

    2nd commit

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+#R11142010　橘内毎歌 
 
 import streamlit as st  
 from textblob import TextBlob
 import pandas as pd
 import altair as alt
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+
 
 # Fxn
 def convert_to_df(sentiment):
@@ -36,67 +39,63 @@ def analyze_token_sentiment(docx):
 
 		
 
+st.image("pic1.png", width=500)
 
 
 
 
 
 def main():
-	st.title("Sentiment Analysis NLP App (感情解析アプリ)")
+	st.title("Sentiment Analysis NLP App")
 	st.subheader("Assignment bonus-1: Streamlit app for Chinese NLP")
 
-	menu = ["Home","About"]
-	choice = st.sidebar.selectbox("Menu",menu)
-
-	if choice == "Home":
-		st.subheader("Home")
-		with st.form(key='nlpForm'):
-			raw_text = st.text_area("Enter Text Here👇")
-			submit_button = st.form_submit_button(label='Analyze')
+	
+	with st.form(key='nlpForm'):
+		raw_text = st.text_area("Enter Text Here👇")
+		submit_button = st.form_submit_button(label='Analyze')
 
 		# layout
-		col1,col2 = st.columns(2)
-		if submit_button:
+	col1,col2 = st.columns(2)
+	if submit_button:
 
-			with col1:
-				st.info("Results")
-				sentiment = TextBlob(raw_text).sentiment
-				st.write(sentiment)
+		with col1:
+			st.info("Results")
+			sentiment = TextBlob(raw_text).sentiment
+			st.write(sentiment)
 
 				# Emoji
-				if sentiment.polarity > 0:
-					st.markdown("Sentiment:: Positive :smiley: ")
-				elif sentiment.polarity < 0:
-					st.markdown("Sentiment:: Negative :angry: ")
-				else:
-					st.markdown("Sentiment:: Neutral 😐 ")
+			if sentiment.polarity > 0:
+				st.markdown("Sentiment:: Positive :smiley: ")
+			elif sentiment.polarity < 0:
+				st.markdown("Sentiment:: Negative :angry: ")
+			else:
+				st.markdown("Sentiment:: Neutral 😐 ")
 
-				# Dataframe
-				result_df = convert_to_df(sentiment)
-				st.dataframe(result_df)
+			# Dataframe
+			result_df = convert_to_df(sentiment)
+			st.dataframe(result_df)
 
 				# Visualization
-				c = alt.Chart(result_df).mark_bar().encode(
-					x='metric',
-					y='value',
-					color='metric')
-				st.altair_chart(c,use_container_width=True)
+			c = alt.Chart(result_df).mark_bar().encode(
+				x='metric',
+				y='value',
+				color='metric')
+			st.altair_chart(c,use_container_width=True)
 
 
 
-			with col2:
-				st.info("Token Sentiment")
+		with col2:
+			st.info("Token Sentiment")
 
-				token_sentiments = analyze_token_sentiment(raw_text)
-				st.write(token_sentiments)
-
-
+			token_sentiments = analyze_token_sentiment(raw_text)
+			st.write(token_sentiments)
 
 
 
 
-	else:
-		st.subheader("About")
+
+
+	
 
 
 if __name__ == '__main__':
